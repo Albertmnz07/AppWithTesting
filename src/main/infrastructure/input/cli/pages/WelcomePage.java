b70c3c6fc14a420dface21d726eedb4ea5973c38
Albertmnz07 <albertomunoz.0803@gmail.com>
2025-12-18 17:18:48 +0100

    Fixed bug in Welcome Page

--- src/main/infrastructure/input/cli/pages/WelcomePage.java
+++ src/main/infrastructure/input/cli/pages/WelcomePage.java
@@ -24,6 +24,7 @@ public class WelcomePage {
 
         switch (selection){
             case LOGIN -> handleLogIn();
+            case CREATE_ACCOUNT -> handleCreateAccount();
             case EXIT -> runner.exit();
         }
     }
