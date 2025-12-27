14b0c6e750eb03a749218bfbbbcb7272668872ed
Albertmnz07 <albertomunoz.0803@gmail.com>
2025-12-27 14:39:18 +0100

    Test happy path for start chat

--- src/test/infrastructure/input/cli/pages/HomePageTest.java
+++ src/test/infrastructure/input/cli/pages/HomePageTest.java
@@ -1,6 +1,9 @@
 package infrastructure.input.cli.pages;
 
 import main.application.ports.InputPort;
+import main.application.usecases.chat.CreateChatUseCase;
+import main.application.usecases.user.FindUserByUserNameUseCase;
+import main.domain.entities.Chat;
 import main.domain.entities.User;
 import main.domain.valueObject.UserName;
 import main.infrastructure.input.cli.ConsoleRunner;
@@ -12,9 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import main.temporalUtils.TestConstants;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -24,7 +26,7 @@ class HomePageTest {
     ConsoleRunner runner;
 
     @Mock
-    User user;
+    User currentUser;
 
     @Mock
     InputPort input;
@@ -32,14 +34,37 @@ class HomePageTest {
     @Mock
     UserName username;
 
+    @Mock
+    FindUserByUserNameUseCase findUserByUserNameUseCase;
+
+    @Mock
+    CreateChatUseCase createChatUseCase;
+
     HomePage homePage;
     @BeforeEach
     void setUp(){
-        when(runner.getCurrentUser()).thenReturn(user);
-        when(user.getUserName()).thenReturn(username);
+        when(runner.getCurrentUser()).thenReturn(currentUser);
+        when(currentUser.getUserName()).thenReturn(username);
         when(username.getValue()).thenReturn(TestConstants.USER_NAME);
 
         homePage = new HomePage(runner , input);
+    }
+
+    @Test
+    void shouldStartNewChat(){
+        when(input.readInt(any())).thenReturn(HomePage.START_CHAT);
+        User searchUser = mock(User.class);
+        when(input.readString(any())).thenReturn(TestConstants.USER_NAME);
+
+        when(runner.getFindUserByUserNameUseCase()).thenReturn(findUserByUserNameUseCase);
+        when(findUserByUserNameUseCase.execute(currentUser.getUserId() , TestConstants.USER_NAME)).thenReturn(searchUser);
+
+        when(runner.getCreateChatUseCase()).thenReturn(createChatUseCase);
+        when(createChatUseCase.execute(currentUser.getUserId() , searchUser.getUserId())).thenReturn(mock(Chat.class));
+
+        homePage.show();
+        verify(createChatUseCase).execute(currentUser.getUserId() , searchUser.getUserId());
+
     }
 
     @Test
