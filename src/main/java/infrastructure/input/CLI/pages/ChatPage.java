e9e2458a4bb5edbeba5d8903e6c972357b380901
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-01-04 21:13:07 +0100

    Build main constructor for page

--- src/main/java/infrastructure/input/CLI/pages/ChatPage.java
+++ src/main/java/infrastructure/input/CLI/pages/ChatPage.java
@@ -1,8 +1,25 @@
 package infrastructure.input.CLI.pages;
 
+import application.ports.InputPort;
+import application.usecases.message.SendMessageUseCase;
+import infrastructure.input.CLI.utils.SessionContext;
+import infrastructure.utils.MessageProvider;
+
 public class ChatPage {
 
-    public ChatPage(){
+    private final SessionContext sessionContext;
+    private final InputPort port;
+    private final MessageProvider messageProvider;
+    private final SendMessageUseCase sendMessageUseCase;
+
+    public ChatPage(SessionContext sessionContext, InputPort port, MessageProvider messageProvider, SendMessageUseCase sendMessageUseCase) {
+        this.sessionContext = sessionContext;
+        this.port = port;
+        this.messageProvider = messageProvider;
+        this.sendMessageUseCase = sendMessageUseCase;
 
     }
+
+
+
 }
