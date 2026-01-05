adcd3cced44d0ed6920229f0adadcdf52682b233
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-01-05 11:52:21 +0100

    Refactor of methods

--- src/main/java/infrastructure/utils/MessageProvider.java
+++ src/main/java/infrastructure/utils/MessageProvider.java
@@ -3,9 +3,7 @@ package infrastructure.utils;
 import domain.exceptions.DomainException;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
-
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 @Component
 public class MessageProvider {
@@ -21,29 +19,11 @@ public class MessageProvider {
         return Locale.getDefault();
     }
 
-    /**
-     * Obtains a text in the correct language. Is independent of errors or simple plain text.
-     * This function exists because calling other functions will be easier separating responsibilities.
-     * @param bundleName Defines the type of text(error, simple text, etc)
-     * @param key the code of the text that is required
-     * @return the {@link String} ready to be displayed
-     */
-    private String getRawText(String bundleName , String key){
-        try{
-            ResourceBundle bundle = ResourceBundle.getBundle(bundleName , currentLocale);
-            return bundle.getString(key);
-        } catch (Exception e){
-            return "Missing key: " + key;
-        }
-    }
-
     public String getError(DomainException code){
-        String pattern = getRawText(ERROR_BUNDLE , code.getCode().toString());
-        return pattern; //is expected to add an implementation to include variables
+        return messageSource.getMessage(code.getCode().toString() , null , getCurrentLocale());
     }
 
-    public String getMessage(String text){
-        String pattern = getRawText(MESSAGE_BUNDLE , text);
-        return pattern;
+    public String getMessage(String text , Object... args){
+        return messageSource.getMessage(text , args , getCurrentLocale());
     }
 }
