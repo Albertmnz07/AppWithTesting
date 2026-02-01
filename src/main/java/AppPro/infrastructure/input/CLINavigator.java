039f564cb0b4495140040f5ddfc64f203df1ced3
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-01 16:08:26 +0100

    Creation of navigator for CLI

---
+++ src/main/java/AppPro/infrastructure/input/CLINavigator.java
@@ -0,0 +1,31 @@
+package AppPro.infrastructure.input;
+
+import AppPro.infrastructure.input.CLI.pages.CLIPage;
+import com.googlecode.lanterna.screen.Screen;
+import org.springframework.context.ApplicationContext;
+
+import java.io.IOException;
+
+public class CLINavigator {
+
+    private final ApplicationContext context;
+    private final Screen screen;
+
+    public CLINavigator(ApplicationContext context, Screen screen) {
+        this.context = context;
+        this.screen = screen;
+    }
+
+    public void goToPage(Class<? extends CLIPage> page){
+        context.getBean(page).show();
+    }
+
+    public void exitApp(){
+        try{
+            screen.stopScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
+    }
+}
