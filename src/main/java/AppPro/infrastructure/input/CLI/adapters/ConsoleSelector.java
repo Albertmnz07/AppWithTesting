85b64e92290cc0ee9d591c76ef470f2cffae240c
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-01 15:22:46 +0100

    Function for render console selector

--- src/main/java/AppPro/infrastructure/input/CLI/adapters/ConsoleSelector.java
+++ src/main/java/AppPro/infrastructure/input/CLI/adapters/ConsoleSelector.java
@@ -1,7 +1,10 @@
 package AppPro.infrastructure.input.CLI.adapters;
 
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +20,30 @@ public class ConsoleSelector {
         this.screen = screen;
         this.title = title;
     }
-    public ConsoleSelector add(Runnable action , String label){
+    public ConsoleSelector addItem(Runnable action , String label){
         itemList.add(new MenuItem(action , label));
         return this;
+    }
+
+    public void render(int selectedItem) throws IOException {
+        screen.clear();
+        TextGraphics tg = screen.newTextGraphics();
+
+        tg.setForegroundColor(TextColor.ANSI.BLACK);
+        tg.putString(1 , 1, title);
+
+        for (int i = 0 ; i < itemList.size() ; i++){
+            if (i == selectedItem){
+                tg.setBackgroundColor(TextColor.ANSI.BLACK);
+                tg.setForegroundColor(TextColor.ANSI.WHITE);
+                tg.putString(1 , 1 + i , itemList.get(i).label());
+            } else{
+                tg.setBackgroundColor(TextColor.ANSI.WHITE);
+                tg.setForegroundColor(TextColor.ANSI.BLACK);
+                tg.putString(1 , 1 + i , itemList.get(i).label());
+            }
+        }
+
+        screen.refresh();
     }
 }
