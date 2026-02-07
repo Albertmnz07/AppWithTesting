295b8d1e1fedf30fb6fac41c323c48b58b2e1cf2
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-08 00:28:55 +0100

    Implements base structure for ConsoleScrollingForm

--- src/main/java/AppPro/infrastructure/input/CLI/adapters/ConsoleScrollingForm.java
+++ src/main/java/AppPro/infrastructure/input/CLI/adapters/ConsoleScrollingForm.java
@@ -1,5 +1,35 @@
 package AppPro.infrastructure.input.CLI.adapters;
 
 
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
 public class ConsoleScrollingForm {
+
+    private final Screen screen;
+    private final TextGraphics tg;
+    private int currentY = 0;
+
+    public ConsoleScrollingForm(Screen screen){
+        this.screen = screen;
+        this.tg = screen.newTextGraphics();
+        clear();
+    }
+
+
+    public void clear(){
+        screen.clear();
+        currentY = 1;
+        refresh();
+    }
+
+    public void refresh(){
+        try{
+            screen.refresh();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
