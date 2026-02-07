242d9059987bf7a4d9de2a09e37d8a5f6b2d6e52
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-07 13:17:38 +0100

    Javadoc for LanternaConfig

--- src/main/java/AppPro/infrastructure/input/CLI/config/LanternaConfig.java
+++ src/main/java/AppPro/infrastructure/input/CLI/config/LanternaConfig.java
@@ -10,16 +10,41 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
+/**
+ * Spring Configuration for Lanterna Terminal User Interface (TUI) components.
+ * <p>
+ *     This class serves aas the central factory for Lanterna resources. Its primary objetive is to integrate
+ *     terminal hardware abstraction into the Spring ApplicationContext, ensuring thread-safe access to the
+ *     console and proper lifecycle management.
+ * </p>
+ */
 @Configuration
 public class LanternaConfig {
 
+    /**
+     * Initializes and provides a singleton {@link Screen} bean.
+     * <p>
+     *     The {@code destroyMehod = "close"} attribute is critical. It ensures that upon application shutdown
+     *     (graceful or otherwise), Spring invokes the terminal's restoration routine. This prevents "terminal
+     *     corruption" by returning the user's console to its original state(restoring the cursor,
+     *     color, and standard input mode);
+     * </p>
+     *
+     * @return A pre-initialized {@link Screen} instance ready for rendering.
+     * @throws IOException If the underlying physical terminal cannot be accessed.
+     */
     @Bean(destroyMethod = "close")
     public Screen lanternaScreen() throws IOException{
+
+        //Detect and create the physical terminal connection(depending of OS)
         Terminal terminal = new DefaultTerminalFactory().createTerminal();
 
+        //Wrap the terminal in a logical screen layer for optimized rendering.
         Screen screen = new TerminalScreen(terminal);
 
         screen.startScreen();
+
+        //Hides the cursor(optional)
         screen.setCursorPosition(null);
 
         return screen;
