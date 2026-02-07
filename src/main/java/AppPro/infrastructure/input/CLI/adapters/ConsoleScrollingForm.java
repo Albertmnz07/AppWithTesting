70d80ab6c82ff9ed1a5dcd497126cd05c7adf830
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-08 00:47:00 +0100

    Funcion for scrolling ConsoleScrollingForm

--- src/main/java/AppPro/infrastructure/input/CLI/adapters/ConsoleScrollingForm.java
+++ src/main/java/AppPro/infrastructure/input/CLI/adapters/ConsoleScrollingForm.java
@@ -1,6 +1,8 @@
 package AppPro.infrastructure.input.CLI.adapters;
 
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 
@@ -18,6 +20,17 @@ public class ConsoleScrollingForm {
         clear();
     }
 
+    public void checkScroll(){
+        TerminalSize size = screen.getTerminalSize();
+        int rows = size.getRows();
+        int cols = size.getColumns();
+
+        if (rows >= currentY - 1){
+            screen.scrollLines(0 , rows -1 , 1);
+            currentY = rows - 2;
+            tg.putString(2 , currentY , " ".repeat(cols));
+        }
+    }
 
     public void clear(){
         screen.clear();
