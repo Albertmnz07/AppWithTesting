80785b8775bf9669fb4800bd97c06aebf3ca9e65
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-08 01:37:58 +0100

    Message prester to show info in Console Scrolling Forms

--- src/main/java/AppPro/infrastructure/input/CLI/utils/LanternaMessagePresenter.java
+++ src/main/java/AppPro/infrastructure/input/CLI/utils/LanternaMessagePresenter.java
@@ -1,7 +1,28 @@
 package AppPro.infrastructure.input.CLI.utils;
 
+import AppPro.domain.exceptions.DomainException;
+import AppPro.infrastructure.input.CLI.adapters.ConsoleScrollingForm;
+import AppPro.infrastructure.utils.MessageProvider;
+import com.googlecode.lanterna.TextColor;
+
 /**
  * Orchestrates the visual representation of messages and errors using colors.
  */
 public class LanternaMessagePresenter {
+
+    private final MessageProvider messageProvider;
+
+    public LanternaMessagePresenter(MessageProvider messageProvider){
+        this.messageProvider = messageProvider;
+    }
+
+    public void printError(ConsoleScrollingForm form , DomainException e){
+        String localizedMessage = messageProvider.getError(e);
+        form.printLine(localizedMessage , TextColor.ANSI.RED);
+    }
+
+    public void printInfo(ConsoleScrollingForm form , String key){
+        String message = messageProvider.getMessage(key);
+        form.printLine(message , TextColor.ANSI.GREEN);
+    }
 }
