e80d65fc55632de2003caa5663109b268c1e85ac
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-09 17:22:53 +0100

    Lanterna configuration to create a normal terminal(production) or "emulated" terminal(IDE)

--- src/main/java/AppPro/infrastructure/input/CLI/config/LanternaConfig.java
+++ src/main/java/AppPro/infrastructure/input/CLI/config/LanternaConfig.java
@@ -4,48 +4,54 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 import java.io.IOException;
 
 /**
  * Spring Configuration for Lanterna Terminal User Interface (TUI) components.
  * <p>
- *     This class serves aas the central factory for Lanterna resources. Its primary objetive is to integrate
- *     terminal hardware abstraction into the Spring ApplicationContext, ensuring thread-safe access to the
- *     console and proper lifecycle management.
+ * This class serves aas the central factory for Lanterna resources. Its primary objetive is to integrate
+ * terminal hardware abstraction into the Spring ApplicationContext, ensuring thread-safe access to the
+ * console and proper lifecycle management.
  * </p>
  */
 @Configuration
 public class LanternaConfig {
 
-    /**
-     * Initializes and provides a singleton {@link Screen} bean.
-     * <p>
-     *     The {@code destroyMehod = "close"} attribute is critical. It ensures that upon application shutdown
-     *     (graceful or otherwise), Spring invokes the terminal's restoration routine. This prevents "terminal
-     *     corruption" by returning the user's console to its original state(restoring the cursor,
-     *     color, and standard input mode);
-     * </p>
-     *
-     * @return A pre-initialized {@link Screen} instance ready for rendering.
-     * @throws IOException If the underlying physical terminal cannot be accessed.
-     */
     @Bean(destroyMethod = "close")
-    public Screen lanternaScreen() throws IOException{
+    public Screen lanternaScreen() throws IOException {
 
-        //Detect and create the physical terminal connection(depending of OS)
-        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        try {
+            Terminal terminal = createTerminalBasedOnEnvironment();
 
-        //Wrap the terminal in a logical screen layer for optimized rendering.
-        Screen screen = new TerminalScreen(terminal);
+            Screen screen = new TerminalScreen(terminal);
+            screen.startScreen();
+            screen.setCursorPosition(null);
 
-        screen.startScreen();
+            return screen;
+        } catch (IOException e) {
+            throw new RuntimeException("Critical fail");
+        }
 
-        //Hides the cursor(optional)
-        screen.setCursorPosition(null);
+    }
 
-        return screen;
+    public Terminal createTerminalBasedOnEnvironment() throws IOException {
+        DefaultTerminalFactory factory = new DefaultTerminalFactory();
+
+        if (System.console() == null) {
+            return factory.setForceAWTOverSwing(true)
+                    .setTerminalEmulatorTitle("AppPro - Developing mode")
+                    .createTerminal();
+        } else {
+            Terminal terminal = factory.setForceTextTerminal(true)
+                    .createTerminal();
+
+            return terminal;
+        }
+
     }
 }
