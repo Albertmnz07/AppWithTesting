9618270f7074df4a6819d4c6e1d54167b172e4de
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-09 17:21:32 +0100

    Property changed in system to be able Springboot application creating apps with GUI

--- src/main/java/AppPro/Main.java
+++ src/main/java/AppPro/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.setProperty("java.awt.headless" , "false");
+
         SpringApplication.run(Main.class , args);
     }
 
