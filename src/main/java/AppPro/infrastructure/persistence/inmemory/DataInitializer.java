e1d6c942cc9e342201fa434aeac9f747bb0ef0a8
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-12 19:59:44 +0100

    Created data initializer to initialize data for test

---
+++ src/main/java/AppPro/infrastructure/persistence/inmemory/DataInitializer.java
@@ -0,0 +1,34 @@
+package AppPro.infrastructure.persistence.inmemory;
+
+import AppPro.domain.entities.User;
+import AppPro.domain.repositories.ChatRepository;
+import AppPro.domain.repositories.MessageRepository;
+import AppPro.domain.repositories.UserRepository;
+import AppPro.domain.valueObject.Password;
+import AppPro.domain.valueObject.UserId;
+import AppPro.domain.valueObject.UserName;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(1)
+public class DataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final ChatRepository chatRepository;
+    private final MessageRepository messageRepository;
+
+    public DataInitializer(UserRepository userRepository, ChatRepository chatRepository, MessageRepository messageRepository) {
+        this.userRepository = userRepository;
+        this.chatRepository = chatRepository;
+        this.messageRepository = messageRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.save(new User(new UserName("Albert") , new Password("1234") , UserId.generate()));
+
+    }
+}
