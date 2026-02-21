4a0c43b5c633d82f7e425c6b4ab5850e3eb17ec6
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-22 00:48:44 +0100

    Created builder for search selector in ui manager

--- src/main/java/AppPro/infrastructure/input/CLI/services/UIManager.java
+++ src/main/java/AppPro/infrastructure/input/CLI/services/UIManager.java
@@ -3,9 +3,12 @@ package AppPro.infrastructure.input.CLI.services;
 import AppPro.domain.exceptions.DomainException;
 import AppPro.infrastructure.input.CLI.adapters.ConsoleScrollingForm;
 import AppPro.infrastructure.input.CLI.adapters.ConsoleSelector;
+import AppPro.infrastructure.input.CLI.adapters.SearchSelector;
 import AppPro.infrastructure.input.CLI.utils.LanternaMessagePresenter;
 import com.googlecode.lanterna.screen.Screen;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 //Manager independent from displays technology
 @Service
@@ -25,6 +28,12 @@ public class UIManager {
 
     public ConsoleScrollingForm createForm(){
         return new ConsoleScrollingForm(screen);
+    }
+
+    public SearchSelector<T> createSearchSelector(Function<String , List<T>> searchProvider
+                                                  , Function<T , String> labelProvider
+                                                  , Consumer<T> onSelectedAction){
+        return new SearchSelector<>(this.screen , searchProvider , labelProvider , onSelectedAction);
     }
 
     public void showError(ConsoleScrollingForm form , DomainException e){
