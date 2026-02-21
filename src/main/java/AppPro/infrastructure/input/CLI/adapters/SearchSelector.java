3716eb728046404dd1008d077ca97ba16aad7b5b
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-21 01:42:01 +0100

    Main body for search selector

---
+++ src/main/java/AppPro/infrastructure/input/CLI/adapters/SearchSelector.java
@@ -0,0 +1,51 @@
+package AppPro.infrastructure.input.CLI.adapters;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class SearchSelector<T> {
+
+    private final Screen screen;
+    private final TextGraphics tg;
+
+    //providers
+    private final Function<String , List<T>> searchProvider;
+    private final Function<T , String> labelProvider;
+    private final Consumer<T> onSelectAction;
+
+    //component state
+    private String currentQuery = "";
+    private List<T> currentRestults = new ArrayList<>();
+    private int selectedItem = 0;
+    private int scrollOffset = 0;
+
+    public SearchSelector(Screen screen,
+                          Function<String, List<T>> searchProvider,
+                          Function<T, String> labelProvider,
+                          Consumer<T> onSelectAction) {
+        this.screen = screen;
+        this.tg = screen.newTextGraphics();
+        this.searchProvider = searchProvider;
+        this.labelProvider = labelProvider;
+        this.onSelectAction = onSelectAction;
+    }
+
+    private void refreshResults(){
+        List<T> rawResults = searchProvider.apply(currentQuery);
+
+        rawResults.sort(Comparator.comparingInt(item -> labelProvider.apply(item).length()));
+
+        this.currentRestults = rawResults;
+
+        this.selectedItem = 0;
+        this.scrollOffset = 0;
+    }
+
+
+}
