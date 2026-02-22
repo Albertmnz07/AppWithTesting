718a31df1787c2393f3a0cb6ff7f0cbe1c0da0ec
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-22 11:42:01 +0100

    Generated other stack of test users(by gemini)

--- src/main/java/AppPro/infrastructure/persistence/inmemory/DataInitializer.java
+++ src/main/java/AppPro/infrastructure/persistence/inmemory/DataInitializer.java
@@ -50,5 +50,36 @@ public class DataInitializer implements CommandLineRunner {
         userRepository.save(new User(new UserName("Elena"), new Password("123"), UserId.generate()));
         userRepository.save(new User(new UserName("Edu"), new Password("123"), UserId.generate()));
 
+        userRepository.save(new User(new UserName("Fran"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Frank"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Franco"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Francisco"), new Password("123"), UserId.generate()));
+
+        userRepository.save(new User(new UserName("Gabi"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Gael"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Gabriel"), new Password("123"), UserId.generate()));
+
+// Usuarios con prefijo "Lu" (ideales para probar nombres muy cortos vs largos)
+        userRepository.save(new User(new UserName("Lu"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Luis"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Lucas"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Lucía"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Luciano"), new Password("123"), UserId.generate()));
+
+        userRepository.save(new User(new UserName("Mar"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Marc"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("María"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Marina"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Mariano"), new Password("123"), UserId.generate()));
+
+        userRepository.save(new User(new UserName("Hugo"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Iván"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Javi"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Lola"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Nuria"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Oscar"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Pablo"), new Password("123"), UserId.generate()));
+        userRepository.save(new User(new UserName("Sara"), new Password("123"), UserId.generate()));
+
     }
 }
