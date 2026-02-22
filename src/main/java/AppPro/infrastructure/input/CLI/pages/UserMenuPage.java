0a94fc695c9d010c28cc4a36b955b6faa94741ee
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-22 11:27:07 +0100

    Implemented navigation to search users system

--- src/main/java/AppPro/infrastructure/input/CLI/pages/UserMenuPage.java
+++ src/main/java/AppPro/infrastructure/input/CLI/pages/UserMenuPage.java
@@ -3,7 +3,9 @@ package AppPro.infrastructure.input.CLI.pages;
 import AppPro.infrastructure.input.CLI.base.AbstractSelectorPage;
 import AppPro.infrastructure.input.CLI.services.CLINavigator;
 import AppPro.infrastructure.input.CLI.services.UIManager;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserMenuPage extends AbstractSelectorPage {
 
     public UserMenuPage(UIManager ui, CLINavigator navigator) {
@@ -14,9 +16,10 @@ public class UserMenuPage extends AbstractSelectorPage {
     public void onShow() {
         createSelector("Select an option")
                 .addItem(() -> System.out.println("Unimplemented") , "Check my chats")
-                .addItem(() -> System.out.println("Unimplemented") , "Star new chat")
-                .addItem(() -> System.out.println("Unimplemented") , "Star new chat")
-                .addItem(navigator::goBack, "Exit");
+                .addItem(() -> navigator.goToPage(StarNewChatPage.class) , "Star new chat")
+                .addItem(() -> System.out.println("Unimplemented") , "Configuration")
+                .addItem(navigator::goBack, "Exit")
+                .show();
 
     }
 }
