c7fb92a8d3a6ced9aabf9e65e46048f5b4749528
Albertmnz07 <albertomunoz.0803@gmail.com>
2026-02-22 10:52:35 +0100

    Implemented show error method for abstract form page

--- src/main/java/AppPro/infrastructure/input/CLI/base/AbstractFormPage.java
+++ src/main/java/AppPro/infrastructure/input/CLI/base/AbstractFormPage.java
@@ -1,13 +1,18 @@
 package AppPro.infrastructure.input.CLI.base;
 
+import AppPro.domain.exceptions.DomainException;
 import AppPro.infrastructure.input.CLI.adapters.ConsoleScrollingForm;
 import AppPro.infrastructure.input.CLI.services.CLINavigator;
 import AppPro.infrastructure.input.CLI.services.UIManager;
+import AppPro.infrastructure.input.CLI.utils.LanternaMessagePresenter;
 
 public abstract class AbstractFormPage extends AbstractCLIPage {
 
-    public AbstractFormPage(UIManager ui, CLINavigator navigator) {
+    private final LanternaMessagePresenter presenter;
+
+    public AbstractFormPage(UIManager ui, CLINavigator navigator , LanternaMessagePresenter presenter) {
         super(ui, navigator);
+        this.presenter = presenter;
     }
 
     @Override
@@ -20,5 +25,9 @@ public abstract class AbstractFormPage extends AbstractCLIPage {
 
     protected String readInputOrBack(ConsoleScrollingForm form, String prompt, boolean isPassword) {
         return form.readInput(prompt, isPassword);
+    }
+
+    protected void showError(ConsoleScrollingForm form , DomainException e){
+        presenter.printError(form , e);
     }
 }
